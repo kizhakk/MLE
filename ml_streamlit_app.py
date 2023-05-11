c41aac612c928066044da098bf05a9b08775e85f
Ramu Kizhakkey <kizhakk@gmail.com>
2023-05-10 21:33:25 -0400

    Update ml_streamlit_app.py

--- ml_streamlit_app.py
+++ ml_streamlit_app.py
@@ -43,7 +43,7 @@ def user_input_features():
     input_features.append(churn_age)
     input_features.append(churn_age)
     df = np.array([input_features])
-    return features
+    return input_features
 
 st.sidebar.markdown("---")
 st.subheader('User Input parameters')
