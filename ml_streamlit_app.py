62140ac9c7aa937b874f764386b3a678d77caefd
Ramu Kizhakkey <kizhakk@gmail.com>
2023-05-10 21:32:18 -0400

    Update ml_streamlit_app.py

--- ml_streamlit_app.py
+++ ml_streamlit_app.py
@@ -17,17 +17,14 @@ input_features = []
 def user_input_features():
     churn_creditscore = st.sidebar.slider('Credit Score', 300, 900, 600,25)
     churn_gender = st.sidebar.selectbox('Gender', ["Male", "Female"])
+    churn_gender = 1
     churn_age = st.sidebar.slider('Age', 10, 90, 40, 5)
     churn_geography = st.sidebar.selectbox('Geography', ["France", "Spain", "Germany"])
+    churn_geography = 1
     churn_hascreditcard = st.sidebar.checkbox('Has Credit Card', value = False)
     churn_activemember = st.sidebar.checkbox('Is Active Member', value = False)
-    data = {'creditscore': churn_creditscore,
-            'gender': churn_gender,
-            'age': churn_age,
-            'geography': churn_geography,
-            'hascreditcard': churn_hascreditcard,
-           'isactivemember': churn_activemember}
-    features = pd.DataFrame(data, index=[0])
+    churn_activemember = 1
+   
     input_features.append(churn_creditscore)
     input_features.append(churn_gender)
     input_features.append(churn_age)
