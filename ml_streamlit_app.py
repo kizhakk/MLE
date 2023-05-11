31d3d1988d5ff5eb69db1a29c765fc6b57106edb
Ramu Kizhakkey <kizhakk@gmail.com>
2023-05-11 10:17:55 -0400

    Update ml_streamlit_app.py

--- ml_streamlit_app.py
+++ ml_streamlit_app.py
@@ -71,7 +71,7 @@ input_dict = {
 if st.button('Predict'):
     #Predict
     input_features_float = [float(x) for x in input_features]
-    prediction = churnmodel.predict(np.array([input_features_float]))
+    prediction = churnmodel.predict_proba(np.array([input_features_float]))
     st.subheader('Prediction Probability for Bank Customer Churn is ...')
     st.write(prediction)
 
